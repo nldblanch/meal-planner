a03940ce5852dc411f820bd1b1d72826206bea44
Nathan <nldblanch@gmail.com>
2024-09-18 10:18:36 +0100

    add back button to header when user can go back

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,39 @@
-import { Linking, Text } from "react-native";
-
+import { Appearance, Image, Linking, Pressable, Text, View } from "react-native";
+import { icons } from "@/constants";
+import { router } from "expo-router";
 type HeaderProps = {
   text: string;
   textStyles?: string;
   link?: string;
 };
+type Args = {
+  icon: any;
+};
+const HeaderIcon = ({ icon }: Args) => {
+  const colorScheme = Appearance.getColorScheme()
+  const tintColor = colorScheme === "dark" ? "white" : "black"
+  return (
+    <Pressable className="flex items-center justify-center absolute left-0 p-4" onPress={() => router.back()}>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={tintColor}
+        className="w-6 h-6"
+      />
+    </Pressable>
+  );
+};
 const Header: React.FC<HeaderProps> = ({ text, textStyles, link }) => {
   return (
-    <Text
-      className={`w-full text-center text-4xl mb-4 text-text dark:text-darkText`}
-      onPress={() => (link ? Linking.openURL(link) : null)}
-    >
-      {text}
-    </Text>
+    <View className="w-full flex flex-row items-center mb-4">
+      {router.canGoBack() && <HeaderIcon icon={icons.leftArrow}/>}
+      <Text
+        className={`text-center text-4xl mx-12 h-full grow text-text dark:text-darkText`}
+        onPress={() => (link ? Linking.openURL(link) : null)}
+      >
+        {text}
+      </Text>
+    </View>
   );
 };
 export default Header;
